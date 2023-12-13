44c04d5a599442a2df95155e3702bed177788765
later-gater <l005.tercero@gmail.com>
2023-12-13 16:00:59 -0600

    created find_optimal_output function
    function of wage, rate, and price

--- pycharm/labor_demand.py
+++ pycharm/labor_demand.py
@@ -8,81 +8,21 @@ alpha = 0.75  # labor share
 beta = 0.25  # capital share
 wage = 1.2
 rate = 0.8
+price = 100
 
+def find_optimal_output(variables: list[wage: float, rate: float, price: float], constants: list[A: float, alpha: float, beta: float]):
+    # return constant_rts: Bool, optimal_output: float
+    wage, float, price = variables
+    A, alpha, beta = constants
+    rts = alpha+beta # stands for return to scale. if rts<1 -> decreasing returns to scale. if rts>1 -> increasing returns to scale
+    if rts == 1: # constant returns to scale -> zero economic profit :(
+        return True # TODO: find optimal_output for constant rts
+    c_term = 1/rts
+    a_term = A**(-1/rts)
+    w_term = wage**(alpha/rts)  # double check
+    r_term = rate**(beta/rts)  # double check
+    alpha_beta_term = ((alpha/beta)**(beta/rts)) + ((beta/alpha)**(alpha/rts))  # double check
+    main_term = price*((c_term*a_term*w_term*r_term*alpha_beta_term)**-1)  # double check
+    return False, main_term**(rts/1-rts)
 
-def cobb_douglas(labor, capital, alpha, beta, A):
-    return A * (labor ** alpha) * (capital ** beta)
-
-
-def cost_function(labor, capital, wage, rate):
-    return (labor*wage) + (capital*rate)
-
-
-def neg_profit(variables, alpha, beta, A, wage, rate):
-    labor, capital = variables
-    return -1 * ((A * (labor ** alpha) * (capital ** beta)) - ((labor*wage) + (capital*rate)))
-
-
-L, K = np.meshgrid(np.linspace(1, 10, 100), np.linspace(1, 10, 100), indexing='ij')
-
-W, R = np.meshgrid(np.linspace(0.1, 5, 50), np.linspace(0.1, 5, 50), indexing='ij')
-
-production_values = cobb_douglas(L, K, alpha, beta, A)
-cost_values = cost_function(L, K, wage, rate)
-profit_values = np.subtract(production_values, cost_values)
-
-start_time = time.time()
-max_profit_funcs = np.vectorize(lambda w, r: minimize(neg_profit, np.array([1.0, 1.0]), args=(alpha, beta, A, w, r), method='L-BFGS-B', bounds=[(0.1, None), (0.1, None)])).__call__(W, R)
-
-max_profit_values = np.full_like(W, fill_value=None, dtype=np.float32)
-
-mapped_profits = np.full_like(W, fill_value=None, dtype=np.dtype)
-
-successes = 0
-for i, row in enumerate(max_profit_funcs):
-    for j, cell in enumerate(row):
-        if cell.success:
-            max_profit_values[i, j] = -cell.fun
-            mapped_profits[i, j] = (W[i, j], R[i, j], max_profit_values[i, j])
-            successes += 1
-        else:
-            mapped_profits[i, j] = (W[i, j], R[i, j], None)
-end_time = time.time()
-
-total_time = end_time-start_time
-
-fig = plt.figure()
-#
-# ax = fig.add_subplot(2, 2, 1, projection='3d')
-# ax.plot_surface(L, K, production_values, cmap='viridis')
-# ax.set_xlabel('Labor')
-# ax.set_ylabel("Capital")
-# ax.set_zlabel("Production")
-# ax.set_title("Production Function")
-#
-# ax2 = fig.add_subplot(2, 2, 2, projection='3d')
-# ax2.plot_surface(L, K, cost_values, cmap='viridis')
-# ax2.set_xlabel('Labor')
-# ax2.set_ylabel("Capital")
-# ax2.set_zlabel("Cost")
-# ax2.set_title("Cost Function")
-#
-# ax3 = fig.add_subplot(2, 2, 3, projection='3d')
-# ax3.plot_surface(L, K, profit_values, cmap='viridis')
-# ax3.set_xlabel('Labor')
-# ax3.set_ylabel("Capital")
-# ax3.set_zlabel("Profit")
-# ax3.set_title("Profit Function")
-#
-ax4 = fig.add_subplot(1, 1, 1, projection='3d')
-ax4.plot_surface(W, R, max_profit_values, cmap='viridis')
-ax4.set_xlabel('Wage')
-ax4.set_ylabel("Rate")
-ax4.set_zlabel("Profit")
-ax4.set_title("Profit by wage and rate")
-
-plt.show()
-
-
-time.sleep(10)
-
+pass
